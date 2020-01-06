8457c4a5ed1cc2dd5ee32346e4c5b6f14ea9a6fb
besanamer <bysan.abo@gmail.com>
2020-01-05 23:38:54 -0800

    1

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
-let  rr =""; 
+let  rr ="" , arr =[]; 
  const add2  = document.getElementById("add2");
  const  card2 =  document.getElementById("newMain");
 
@@ -8,16 +8,22 @@ my.append('Content-Type', 'application/json');
     fetch('http://localhost:3000/contact',{
         method:'GET', 
         headers : my })  .then( res => res.json())  .then((data) => { 
+            arr = data ; 
             console.log(data) ; 
-            render(data); });
+            arr.forEach(obj=> {
+                 render(obj); 
+            });
+           
+        });
     
 function render (a){
+
     rr='<div class="maincard">' +
         '<img class="img2" src="../images/alcont.png" alt="Avatar">' +
         '<div class="container">' +
             '<h4><b>The Contact is </b></h4>' +
-            '<p >'+ a.name +'</p>' +
-                '<button class="button"  id="delete" onclick="del (event)"  ><strong>DELETE</strong></button>' +
+            '<p >'+ a.name +a.position  +a.mobile + a.email+'</p>' +
+                '<button class="button"  id="'+ a.id +'"  onclick="del(event)"  ><strong>DELETE</strong></button>' +
             '<br></div><br></div>' ;
             card2.innerHTML += rr ; 
         }
@@ -25,18 +31,16 @@ function render (a){
         // document.getElementById("delete").addEventListener("click" , ()=>{
 
         function del (event){
-            const i = event.target.getAttribute('') ;
-            
-             fetch('http://localhost:3000/contact/',{
+            const i = event.target.getAttribute('id') ;
+
+             fetch('http://localhost:3000/contact/'+i ,{
                 method:'DELETE',
-                headers : my , 
-                body:JSON.stringify({
-                    id : i
-                })
+                headers : my 
                 })
                 .then( res => res.json())
                 .then(data => {  
                 console.log("done  delete"  );  
+                // card2.innerHTML = "" ;
 });
         }
 
