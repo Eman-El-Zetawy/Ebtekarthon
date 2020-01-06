d646fd63983c752ce34cb23d1ec5ccf97d0a57d5
To do list <rrss51@outlook.com>
2020-01-06 10:32:48 +0200

    12

--- js/speaker.js
+++ js/speaker.js
@@ -1,7 +1,9 @@
 document.getElementById('imgInp').addEventListener('click', () => {
     document.getElementById('fileInput').click()
+    
   })
     function readURL(input) {
+        console.log(input.files && input.files[0]);
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             
@@ -11,8 +13,52 @@ document.getElementById('imgInp').addEventListener('click', () => {
             reader.readAsDataURL(input.files[0]);
         }
     }
-    document.getElementById("imgInp").change(function(){
+    document.getElementById("fileInput").addEventListener("change" , function(){
         readURL(this);
-    }); 	
-
-
+    });
+      /////////
+      input   = document.getElementById("fileInput");
+      let b64 = "";
+      input.onchange = function () {
+        var file = input.files[0],
+          img = new FileReader();
+        img.onloadend = function () {
+          b64 = img.result.replace(/^data:.+;base64,/, '');
+          console.log(b64);
+        };
+        img.readAsDataURL(file);
+      };
+  
+    
+  
+  const save = document.getElementById("save");
+  
+  save.addEventListener("keydown",function(e){
+    if(e.key==="Enter"){
+      add();
+    } });
+  
+  save.addEventListener("click" , add);
+   
+      function add(){
+        let SpeakerName =document.getElementById("name").value ;
+        let SpeakerPosition =document.getElementById("position").value ;
+        let SpeakerEmail =document.getElementById("email").value ;
+        let SpeakerBIO =document.getElementById("brif").value ;
+  
+        const myHeader =new Headers();
+        myHeader.append('Content-Type', 'application/json');
+    
+  
+        fetch('http://localhost:3000/speaker',{
+        method:'POST',
+        headers: myHeader,
+        body:JSON.stringify({
+        name : SpeakerName , 
+        position : SpeakerPosition,
+        email : SpeakerEmail,
+        BIO : SpeakerBIO
+     })
+  }).then( response=>response.json()).then(data=> console.log(data));
+  }
+  
