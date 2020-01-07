caf78cbeb16c953b1b1f4679a096994e3479de5c
Rabab Azazmih <55941943+RA95-png@users.noreply.github.com>
2020-01-07 12:44:20 +0200

    UPDATE SPREAKER

---
+++ speaker/addSpeaker.js
@@ -0,0 +1,65 @@
+ 
+    const myHeader =new Headers();
+    myHeader.append('Content-Type', 'application/json');
+    
+    let n= document.getElementById("new");
+    let arr = [];
+    fetch('http://localhost:3000/speaker',{
+      meathod:'GET', 
+      headers : myHeader }).then( response=>response.json()).then(data=> { 
+
+        // let arr = [];
+          console.log(data) ; 
+          arr=data ;
+          arr.forEach(item=> {
+               RenderingSpeaker(item); 
+          });
+         
+      });
+    
+      let NewSpeaker= document.getElementById('newspeaker');
+  
+      function RenderingSpeaker(d){
+          console.log(d);
+          let add=`<div class="card">`+
+          `<img src=""../images/IMG12.jpg"" alt="Avatar">`+
+          `<div class="container">`+
+              `<h4><b>`+d.name+`</b></h4>`+
+              `<p>`+d.bio+`</p><br>`+
+            `<button class="button"  onclick ="DeleteSpeaker(event)" > <strong  mm="`+ d.id +`" > DELETE </strong ></button>
+          </div>
+      </div>`
+  
+      NewSpeaker.innerHTML+= add;
+      
+    }
+
+    function DeleteSpeaker(event)
+    {
+        let l=[];
+        let id = event.target.getAttribute('mm') ;
+        l.push(id);
+        arr.forEach((obj, index )=>{
+if(obj.id==id){
+    arr.splice(index,1);
+}
+        })
+        const myHeader =new Headers();
+        myHeader.append('Content-Type', 'application/json');
+        
+        fetch('http://localhost:3000/speaker/',{
+           method:'DELETE',
+           headers : myHeader , 
+           body:JSON.stringify({
+               id :l
+           })
+           })
+           .then( res => res.json())
+           .then(data => {  
+           console.log(data);  
+           NewSpeaker.innerHTML ="";
+           arr.forEach(obj=>{
+               RenderingSpeaker(obj);           })
+    }
+           )}
+
