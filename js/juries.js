79cbd16f9014d5cf2dff86a6c7d6f7cc666437a1
Rabab Azazmih <55941943+RA95-png@users.noreply.github.com>
2020-01-07 12:46:33 +0200

    UPDATE

--- js/juries.js
+++ js/juries.js
@@ -1,38 +1,75 @@
-$(document).ready( function() {
-    $(document).on('change', '.btn-file :file', function() {
-    var input = $(this),
-        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    input.trigger('fileselect', [label]);
-    });
-
-    $('.btn-file :file').on('fileselect', function(event, label) {
-        
-        var input = $(this).parents('.input-group').find(':text'),
-            log = label;
-        
-        if( input.length ) {
-            input.val(log);
-        } else {
-            if( log ) alert(log);
-        }
+document.getElementById('imgInp').addEventListener('click', () => {
+    document.getElementById('fileInput').click()
     
-    });
+  })
     function readURL(input) {
+        console.log(input.files && input.files[0]);
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             
             reader.onload = function (e) {
-                $('#img-upload').attr('src', e.target.result);
+                document.getElementById("imgInp").setAttribute('src', e.target.result);
             }
-            
             reader.readAsDataURL(input.files[0]);
         }
     }
-
-    $("#imgInp").change(function(){
+    document.getElementById("fileInput").addEventListener("change" , function(){
         readURL(this);
-    }); 	
-});
+    });
+      /////////
+      input   = document.getElementById("fileInput");
+      let b64 = "";
+      input.onchange = function () {
+        var file = input.files[0],
+          img = new FileReader();
+        img.onloadend = function () {
+          b64 = img.result.replace(/^data:.+;base64,/, '');
+          console.log(b64);
+        };
+        img.readAsDataURL(file);
+      };
+  
+    
+  
+  const save = document.getElementById("save");
+  
+  save.addEventListener("keydown",function(e){
+    if(e.key==="Enter"){
+      add();
+    } });
+  
+  save.addEventListener("click" , add);
+  let array=[];
+   
+      function add(){
+        let Name =document.getElementById("name").value ;
+        let Position =document.getElementById("position").value ;
+        let Email =document.getElementById("email").value ;
+        let JurisBIO =document.getElementById("brif").value ;
+  
+        const myHeader =new Headers();
+        myHeader.append('Content-Type', 'application/json');
+    
+  
+        fetch('http://localhost:3000/juries',{
+        method:'POST',
+        headers: myHeader,
+        body:JSON.stringify({
+        name : Name , 
+        position : Position,
+        email : Email,
+        BIO : JurisBIO,
+        img :"test"
+     })
+  }).then( response=>response.json()).then(data=> console.log(data));
+  
+  }
+
+
+
+
+
+
 function create() {
     location.replace("../juries/Juries2.html")
 }
--- js/speaker.js
+++ js/speaker.js
@@ -1,87 +1,68 @@
-
-//     var fileTag = document.getElementById("fileInput");
-//     preview = document.getElementById("imgInp"); 
-
-//     fileTag.addEventListener("change", function() {
-//         preview.addEventListener('click', () => 
-//         {    fileTag.click()  ;
-//      changeImage(this);
-//     }
-// )});
-//     function changeImage(input) {
-//      var reader;
-//      console.log(input);
-//      if (input.files && input.files[0]) {
-//        reader = new FileReader();
-      
-//        reader.onload = function(e) {
-//          preview.setAttribute('src', e.target.result);
-//        }
-//        reader.readAsDataURL(input.files[0]);
-//      }
-//     }
-    
-//      document.getElementById('save').addEventListener('click',add);
 document.getElementById('imgInp').addEventListener('click', () => {
-  document.getElementById('fileInput').click()
-  
-})
-  function readURL(input) {
-      console.log(input.files && input.files[0]);
-      if (input.files && input.files[0]) {
-          var reader = new FileReader();
-          
-          reader.onload = function (e) {
-              document.getElementById("imgInp").setAttribute('src', e.target.result);
-          }
-          reader.readAsDataURL(input.files[0]);
-      }
-  }
-  document.getElementById("fileInput").addEventListener("change" , function(){
-      readURL(this);
-  });
-    /////////
-    input   = document.getElementById("fileInput");
-    let b64 = "";
-    input.onchange = function () {
-      var file = input.files[0],
-        img = new FileReader();
-      img.onloadend = function () {
-        b64 = img.result.replace(/^data:.+;base64,/, '');
-        console.log(b64);
+    document.getElementById('fileInput').click()
+    
+  })
+    function readURL(input) {
+        console.log(input.files && input.files[0]);
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                document.getElementById("imgInp").setAttribute('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    document.getElementById("fileInput").addEventListener("change" , function(){
+        readURL(this);
+    });
+      /////////
+      input   = document.getElementById("fileInput");
+      let b64 = "";
+      input.onchange = function () {
+        var file = input.files[0],
+          img = new FileReader();
+        img.onloadend = function () {
+          b64 = img.result.replace(/^data:.+;base64,/, '');
+          console.log(b64);
+        };
+        img.readAsDataURL(file);
       };
-      img.readAsDataURL(file);
-    };
-
   
-
-const save = document.getElementById("save");
-
-save.addEventListener("keydown",function(e){
-  if(e.key==="Enter"){
-    add();
-  } });
-
-save.addEventListener("click" , add());
- 
-    function add(){
-      let SpeakerName =document.getElementById("name").value ;
-      let SpeakerPosition =document.getElementById("position").value ;
-      let SpeakerEmail =document.getElementById("email").value ;
-      let SpeakerBIO =document.getElementById("brif").value ;
-
-      const myHeader =new Headers();
-      myHeader.append('Content-Type', 'application/json');
+    
   
+  const save = document.getElementById("save");
+  
+  save.addEventListener("keydown",function(e){
+    if(e.key==="Enter"){
+      add();
+    } });
+  
+  save.addEventListener("click" , add);
+  let array=[];
+   
+      function add(){
+        let SpeakerName =document.getElementById("name").value ;
+        let SpeakerPosition =document.getElementById("position").value ;
+        let SpeakerEmail =document.getElementById("email").value ;
+        let SpeakerBIO =document.getElementById("brif").value ;
+  
+        const myHeader =new Headers();
+        myHeader.append('Content-Type', 'application/json');
+    
+  
+        fetch('http://localhost:3000/speaker',{
+        method:'POST',
+        headers: myHeader,
+        body:JSON.stringify({
+        name : SpeakerName , 
+        position : SpeakerPosition,
+        email : SpeakerEmail,
+        BIO : SpeakerBIO,
+        img :"test"
+     })
+  }).then( response=>response.json()).then(data=> console.log(data));
+  
+  }
 
-      fetch('http://localhost:3000/speaker',{
-      method:'POST',
-      headers: myHeader,
-      body:JSON.stringify({
-      name : SpeakerName , 
-      position : SpeakerPosition,
-      email : SpeakerEmail,
-      BIO : SpeakerBIO
-   })
-}).then( response=>response.json()).then(data=> console.log(data));
-}
+
