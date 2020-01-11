b254c8321bacff2ae2f91d760ac8797a238d999b
hamzaOly <48622633+hamzaOly@users.noreply.github.com>
2020-01-11 09:27:16 +0200

    day

--- js/schedule.js
+++ js/schedule.js
@@ -5,8 +5,118 @@ function levelTwo() {
 function addEvent() {
 			location.replace("../schedules/addEvent.html");
 }
-// hamza trash>>> here for deleting the current day from shcedule, data base get and delete method.
-function removeDay() {
-	 var day = document.getElementById('day');
-    day.remove();
+// function removeDay() {
+// 	 var day = document.getElementById('day');
+//     day.remove();
+// }
+let str = "";
+const day = document.getElementById('day');
+const myheader = new Headers();
+myheader.append('Content-Type', 'application/json');
+fetch('http://localhost:3000/day', {
+	method: 'GET',
+	headers: myheader
+}).then(response => response.json())
+  .then((data) => {
+	console.log(data);
+	render(data);
+});
+
+function render(ff) {
+	str=
+	'<div class="schedule-post">'+
+				
+				'<input type="button" onclick="delet()" id="deleteDay">'+
+				'<input type="button" onclick="addEvent()" id="edit"><br>'+
+				'<div class="schedule-content">'+
+					'<h3 id="show">'+ff.day_date+'</h3>'+
+					'<hr>'+
+				'</div>'+
+			
+			'</div>'
+	;
+	day.innerHTML += str;
 }
+
+ function delet(){
+	var day = document.getElementById('day');
+	    day.remove();
+             fetch('http://localhost:3000/day',{
+                method:'DELETE',
+                headers : myheader , 
+                body:JSON.stringify({
+                    id : day
+                })
+                })
+                .then( res => res.json())
+                .then(data => {  
+                console.log(`jhvjhv ${data}`);  
+});
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const add = document.getElementById('add').addEventListener('click', render);
+
+//function levelTwo() {
+//			location.replace("../justInCaseschadule/createDay.html");
+//}
+//
+//function addEvent() {
+//			location.replace("../schedules/addEvent.html");
+//}
+//function removeDay() {
+//	 var day = document.getElementById('day');
+//    day.remove();
+//}
+//const str = "";
+//const day = document.getElementById('day')
+////const myheader = new Headers();
+////
+////myheader.append('Content-Type', 'application/json');
+////fetch('http://localhost:3000/day', {
+////	method: 'GET',
+////	headers: myheader
+////}).then(res => res.json())
+////  .then((data) => {
+////	console.log(data);
+////	render(data);
+////});
+//render();
+////const add = document.getElementById('add').addEventListener('click', render);
+//
+//function render() {
+//	`<div class="schedule-post" id="day">`+
+//				`<input type="button" onclick="removeDay(this)" id="deleteDay">`+
+//				`<input type="button" onclick="addEvent()" id="edit"><br>`+
+//				`<div class="schedule-content">`+
+//					`<h3 id="show">Wednesday 12/18</h3>`+
+//					`<hr>`+
+//					`<img src="../images/2.png" alt="" id="break">`+
+//					`<h5>10:00 AM</h5>`+
+//					`<h6>Sport</h6>`+
+//					`<hr>`+
+//					`<img src="../images/2.png" alt="" id="break">`+
+//					`<h5>11:00 AM</h5>`+
+//				`</div>`+
+//			`</div>`;
+//	day.innerHTML += str;
+//}
+//
