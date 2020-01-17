fc257042ad92bf89d07b7a5676068a3e3d8f4c61
eman.el.zetawy <eman.el.zetawy12@gmail.com>
2020-01-17 15:09:23 +0200

    users

--- User_Ebtekarthon/contactUser.js
+++ User_Ebtekarthon/contactUser.js
@@ -1,38 +1,32 @@
-let  arr =[] , i ; 
+var  arrayData = []  ; 
  const  card =  document.getElementById("newMain") ;
-const my =new Headers();
+const myH =new Headers();
 my.append('Content-Type', 'application/json');
+
     fetch('http://localhost:3000/contact',{
         method:'GET', 
-        headers : my }) 
+        headers : myH }) 
         .then( res => res.json())  .then((data) => { 
-            arr = data ; 
+            arrayData = data ; 
             console.log(data) ; 
-              render(); 
+              rendercontact(); 
         });
 
-        function render(){
+        function rendercontact(){
             let r ="";
-             arr.forEach((o,j)=>{ if( o.id == i){arr.splice(j,1); }});
-             console.log(arr);
-                  arr.forEach(a=> { 
-       r +='<div class="col-lg-4 col-md-6">' +
-       '<div class="contact-post">' +
-       '<img src="./Ebtekarthon_files/mem1.jpg" alt="">' +
+             console.log(arrayData);
+             arrayData.forEach(a=> { 
+        r +='<div class="col-lg-4 col-md-6">'+
+        '<div class="contact-post"  >'+
+         '<img src="./Ebtekarthon_files/mem1.jpg" alt="">' +
         '<div class="contact-post-content">'+
-        '<h2>'+ a.name + '</h2><span>crew</span><p>'+a.mobile+
+        '<h2>'+ a.name + '</h2><span>'+a.position+'</span><p>'+a.mobile+
         '<br>'+a.email+'</p></div></div></div>';
-
-    //    '<b> Name : '</b><br>' +
-    //    '<b> Position : '+ a.position+'</b><br><b> Mobile : ' + + "</b><br><b>Email : "+ +
-    //    '</b><button class="button" id ="'+ a.id +'"  onclick="del(this.id)"  ><strong>DELETE</strong></button>' +
-    //    '<br></div><br></div></a>' ;  
        });
                 card.innerHTML = r ;
-               
                }
 								
-								
+							
 									
 									
 									
--- User_Ebtekarthon/partnarsUser.js
+++ User_Ebtekarthon/partnarsUser.js
@@ -1,9 +1,47 @@
-const my = new Headers();
+   let  htmlr  ="" ,  ArrayD=[], arrayData = [] ; 
+    const  create =  document.getElementById("create");
+ const  card =  document.getElementById("newMain") ;
+   const my = new Headers();
 my.append('Content-Type', 'application/json');
+
     fetch('http://localhost:3000/partners',{
         method:'GET', 
-        headers : my })  .then( res => res.json())  .then((data) => { 
+        headers : my }) .then( res => res.json()) .then((data) => { 
             console.log(data) ; 
-            arr=data;
-                render(); 
+            ArrayD=data;
+            render(); 
         });
+
+
+function render(){ 
+htmlr ="";
+ArrayD.forEach(a=> {
+htmlr +='<li><a href="'+a.wsite + '" id="'+a.id+'"><img src="./Ebtekarthon_files/4.png" alt=""></a></li>'; 
+     }); 
+    create.innerHTML = htmlr  ; 
+    }
+
+
+    fetch('http://localhost:3000/contact',{
+        method:'GET', 
+        headers : my }) 
+        .then( res => res.json())  .then((data) => { 
+            arrayData = data ; 
+            console.log(data) ; 
+              rendercontact(); 
+        });
+
+        function rendercontact(){
+            let r ="";
+             console.log(arrayData);
+             arrayData.forEach(a=> { 
+        r +='<div class="col-lg-4 col-md-6">'+
+        '<div class="contact-post"  >'+
+         '<img src="./Ebtekarthon_files/mem1.jpg" alt="">' +
+        '<div class="contact-post-content">'+
+        '<h2>'+ a.name + '</h2><span>'+a.position+'</span><p>'+a.mobile+
+        '<br>'+a.email+'</p></div></div></div>';
+       });
+                card.innerHTML = r ;
+               }
+					
