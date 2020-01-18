a6b6bf2cd65ffaf1529abde02921de6749c32b7c
hamzaOly <48622633+hamzaOly@users.noreply.github.com>
2020-01-18 19:52:31 +0200

    event post & delete

--- js/levelTwoSchedule.js
+++ js/levelTwoSchedule.js
@@ -5,18 +5,26 @@ function levelTwo() {
 function saveDay() {
 		location.replace("../schedules/schedules.html");
 	}
-// hamza trash>>here after we set up the data base,& method post, the main page of schedule we get the result from here.
-function day() {
-	var str="";
-	var createday = document.getElementById('dateDay').value;
-   var show = document.getElementById('show');
-	var newl= document.createElement('li');
-	//if statement
-	createday==''
-	? alert('input is empty'):
-	createday!==''
-	 str= createday;
-	 newl.textContent= str;
-	 show.appendChild(newl);
-}
+const myHeader =new Headers();
+myHeader.append('Content-Type', 'application/json');
+const save = document.getElementById('save').addEventListener('click', dayFactory);
 
+function dayFactory(){
+	const title = document.getElementById('title');
+	const season = document.getElementById('season');
+
+	 fetch('http://localhost:3000/day',{
+	  method:'POST',
+	  headers : myHeader , 
+	  body:JSON.stringify({
+				day_date :  title.value, 
+				ebt_id :  season.value 
+
+	   })
+	}).then( response=>response.json())
+	  .then((data) => {
+		console.log(data); 
+	}); 
+	};
+
+	console.log('fthyt')
